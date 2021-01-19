da71e5d00475ab50c3afd571c76defa9e1a296de
tomas agnese <tagnese1985@gmail.com>
2021-01-19 20:39:50 -0300

    minor changess

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "controller#index"
+  root to: "cocktails#index"
 
   resources :cocktails, only: [:index, :show, :create, :new] do
     resources :doses, only: [:create, :new]
