a880dc949db28f7d5cfb72b02840fd123096f05b
tomas agnese <tagnese1985@gmail.com>
2021-01-21 00:35:51 -0300

    mister cocktails wiht more style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import { initSelect2 } from '../packs/init_select2';
+
+document.addEventListener("turbolinks:load", function() {
+  initSelect2();
+});
